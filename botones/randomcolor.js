8ae82653402e56a6ba3033d29479c295e04adf5e
gabyyeah <gabymoreno181972@gmail.com>
2021-02-07 15:44:13 -0500

    botoneees

--- botones/randomcolor.js
+++ botones/randomcolor.js
@@ -1,6 +1,6 @@
 let boton1 = document.querySelector("#color")
 let parrafo1 = document.querySelector("#parrafo1")
-boton.onclick = cambiarColor
+boton1.onclick = cambiarColor
 function cambiarColor(){
     let a = Math.floor(Math.random() * 226)
     let b = Math.floor(Math.random() * 226)
