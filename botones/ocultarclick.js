9e79417a4ca5759e01cc604632bdc8ec475accb8
gabyyeah <gabymoreno181972@gmail.com>
2021-02-07 16:10:57 -0500

    botones xd

--- botones/ocultarclick.js
+++ botones/ocultarclick.js
@@ -6,7 +6,7 @@ boton2.onclick = ocultaryaparecer
 
 function ocultaryaparecer(){
     if (parrafo2.innerHTML == ""){
-        parrafo2.innerHTML = "parrafo 2 xd ufde dkedfkkmes"
+        parrafo2.innerHTML = "MBTI son las siglas para referirnos al Myers-Briggs Type Indicator, un instrumento diseñado para evaluar la personalidad a partir de nuestras respuestas acerca de cómo nos sentimos o actuamos en situaciones particulares."
     }
     else{
         parrafo2.innerHTML = ""
