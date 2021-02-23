df0cef0f68e53bdcb67d779010e50c385eb5c39e
gabyyeah <gabymoreno181972@gmail.com>
2021-02-23 14:22:57 -0500

    owo

--- paginacontador/contador.js
+++ paginacontador/contador.js
@@ -3,5 +3,7 @@ localStorage.setItem("visitas", contar)
 let contarExistente=localStorage.getItem("visitas")
 do{
     contar=contar+1
+    localStorage.setItem("visitas", contar)
+    contarExistente=localStorage.getItem("visitas")
     alert("Ha visitado esta página "+ contar + " veces")
 } while (contarExistente==contar)
