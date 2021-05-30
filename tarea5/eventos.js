a6df0c365b2f47bfc86ea16256dbfceccc5ae57d
gabyyeah <gabymoreno181972@gmail.com>
2021-05-30 16:00:08 -0500

    a

---
+++ tarea5/eventos.js
@@ -0,0 +1,68 @@
+let canvas = document.querySelector("#miCanvas")
+let ctx = canvas.getContext("2d")
+
+function dibujarPelota(xC, yC, r, color){
+    ctx.beginPath();
+    ctx.strokeStyle = color
+    ctx.fillStyle = color;
+
+    ctx.arc(xC, yC, r, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+}
+let x = 600;
+let y = 0;
+let r = 20;
+let color = "red";
+
+let vY = 10;
+let dT = 30;
+
+function moverYDibujar(){
+    ctx.clearRect(0,0, 900, 700);
+    let dY = dT/1000 *vY
+    y = y + dY;
+
+    dibujarPelota(x, y, r, color);
+}
+
+function arrancarOParar(){
+    if (vY > 0){
+        vY = 0;
+    }
+    else{
+        vY = 10;
+    }
+}
+window.setInterval(moverYDibujar, dT);
+
+window.onkeyup = function(event){
+    console.log(event.code);
+    if (event.code == "Space")
+    {
+        arrancarOParar();
+    }
+    if (event.code == "BracketRight")
+    {
+        r=r+1;
+    }
+    else if (event.code == "Slash")
+    {
+        r=r-1;
+    }
+}
+
+function cambiarColor() {
+ color= "#" + Math.floor(Math.random()*16777215).toString(16)
+}
+window.onclick=function(event){
+    let clickX=event.offsetX
+    let clickY=event.offsetY
+    distanciaPelota= Math.sqrt(Math.pow(clickX-x,2)+ Math.pow(clickY-y,2))
+    if (distanciaPelota<=r){
+        cambiarColor()
+    }
+    else{
+        vY=-vY
+    }
+}
