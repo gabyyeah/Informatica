5ee76a72c7ef7b9fff1879faf1c73bee5ceb31ca
gabyyeah <gabymoreno181972@gmail.com>
2021-07-19 13:51:34 -0500

    ,

--- juego/juego.js
+++ juego/juego.js
@@ -1,57 +1,117 @@
 let canvas = document.querySelector("#miCanvas")
 let ctx = canvas.getContext("2d")
 
+//Finaliza el juego
+//function timeOver() {
+  //clearInterval(cronometro);
+ // function alerta (){
+    //alert("Se acabo tu tiempo");
+   // document.querySelector("#timeOver").classList.add("visible");
+  //} 
+  
+
+  //Inicia el juego
+  document.querySelector("#juego-normal")
+  .addEventListener("click", iniciaCronometro);
+  //document.querySelector("#juego-normal")
+  //.addEventListener("click", draw);
+  
+  //cronometro
+  var cronometro;
+  function iniciaCronometro() {
+      var segundos = 10;
+      var minutos = 0;
+      var segundosTexto;
+      var minutosTexto;
+    
+      function actualizaContador() {
+        segundos--;
+        
+        if (segundos < 0) {
+          segundos = 59;
+          minutos--;
+        }
+        if (minutos < 0) {
+          segundos = 0;
+          minutos = 0;
+          clearInterval(cronometro);
+          timeOver();
+        }
+        segundosTexto = segundos;
+        minutosTexto = minutos;
+        if (segundos < 10) {
+          segundosTexto = "0" + segundos;
+          if (segundos == 0) { 
+            //Finaliza el juego
+            function alerta (){
+              alert("Se acabo tu tiempo");
+            } 
+            alerta()
+           }
+        }
+        if (minutos < 10) {
+          minutosTexto = "0" + minutos;
+        }
+        document.querySelector("#minutos").innerText = minutosTexto;
+        document.querySelector("#segundos").innerText = segundosTexto;
+      }
+      cronometro = setInterval(actualizaContador, 1000);
+    }
+
+
+
 // oso 
-
-function dibujarOso(xC, yC, r, color){
-    ctx.beginPath();
-    ctx.strokeStyle = color
-    ctx.fillStyle = color;
-
-    ctx.arc(xC, yC, r, 0, 2*Math.PI);
-    ctx.stroke();
-    ctx.fill();
-} 
-let x = 40;
-let y = 275;
+let xC = 40;
+let yC = 275;
 let r = 25;
 let color = "maroon";
 
-let vX = 0;
-let dT = 30;
+ctx.beginPath();
+ctx.strokeStyle = color
+ctx.fillStyle = color;
 
-function moverYDibujar(){
-    ctx.clearRect(0,0, 900, 700);
-    let dX = dT/1000 *vX
-    x = x + dX;
-
-    dibujarOso(x, y, r, color);
-}
-
-window.setInterval(moverYDibujar, dT)
+ctx.arc(xC, yC, r, 0, 2*Math.PI);
+ctx.stroke();
+ctx.fill();
 
 window.onkeydown = function(event){
-    console.log(event.code);
     if (event.code == "ArrowRight")
     {
-        x=x+50
+        xC=xC+50
     }
     if (event.code=="ArrowLeft"){
-        x=x-50
+        xC=xC-50
     }
     if (event.code=="ArrowUp"){
-        y=y-50
+        yC=yC-50
     }
     if (event.code=="ArrowDown"){
-        y=y+50
+        yC=yC+50
     }
 }
 
 //bosque
 ctx.beginPath()
-    ctx.strokeStyle='darkgreen'
-    ctx.fillStyle= 'darkgreen'
-    ctx.fillRect(1100,0,400,557)
-    ctx.stroke()
-    ctx.fill()
+ctx.strokeStyle='darkgreen'
+ctx.fillStyle= 'darkgreen'
+ctx.fillRect(1100,0,400,557)
+ctx.stroke()
+ctx.fill()
     
+//Nuevo cazador
+  
+function draw() {
+    let x=0;
+    let vX=2;  
+    ctx.beginPath()
+    ctx.strokStyle='darkgrey';
+    ctx.fillStyle= 'darkgrey';
+    ctx.fillRect(1100,0,-20,50);
+    ctx.moveTo(x,y);
+      if (x>=1260 || x<0){
+      vX=vX*-1;
+      }
+      x=x+vX;
+  }
+  draw();
+  ctx.moveto();
